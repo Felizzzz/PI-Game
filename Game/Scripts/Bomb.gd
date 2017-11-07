32ab0a6f505f2cbe288750a4314f5c8aa38857d3
chenl38147 <cc1297229789@gmail.com>
2017-11-07 14:29:44 +0000

    bombanimation
    bombanimation

--- Game/Scripts/Bomb.gd
+++ Game/Scripts/Bomb.gd
@@ -1,15 +1,23 @@
 extends Area2D
+const IDLE_SPEED=10
 var MOTION=Vector2()
+var BombanimNode
+var anim=""
+
 
 
 func _ready():
 	set_fixed_process(true)
+	BombanimNode = get_node("AnimatedSprite")
 
 func start_at(MOTION):
 	set_pos(MOTION)
 	
 func _fixed_process(delta):
 	set_pos(get_pos())
+	
+	anim="Bomb_anim"
+	BombanimNode.play(anim)
 
 
 func _on_Timer_timeout():
